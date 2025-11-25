5fb2de87c0b5f814eebfa2dffb170dd9a5d342ec
Victor Nwoseh <victor.nwoseh05@gmail.com>
2025-11-25 20:16:43 +0000

    feat(backend): create node handler registry service

---
+++ backend/src/workflows/services/node-handler-registry.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+
+import { INodeHandler } from '../interfaces/node-handler.interface';
+import { HttpRequestHandler } from '../handlers/http-request.handler';
+import { DelayHandler } from '../handlers/delay.handler';
+import { ConditionHandler } from '../handlers/condition.handler';
+import { VariableHandler } from '../handlers/variable.handler';
+import { SlackHandler } from '../handlers/slack.handler';
+import { EmailHandler } from '../handlers/email.handler';
+import { SheetsHandler } from '../handlers/sheets.handler';
+import { WebhookHandler } from '../handlers/webhook.handler';
+
+@Injectable()
+export class NodeHandlerRegistryService {
+  private readonly handlers = new Map<string, INodeHandler>();
+
+  constructor(
+    httpRequestHandler: HttpRequestHandler,
+    delayHandler: DelayHandler,
+    conditionHandler: ConditionHandler,
+    variableHandler: VariableHandler,
+    slackHandler: SlackHandler,
+    emailHandler: EmailHandler,
+    sheetsHandler: SheetsHandler,
+    webhookHandler: WebhookHandler,
+  ) {
+    this.handlers.set('http', httpRequestHandler);
+    this.handlers.set('delay', delayHandler);
+    this.handlers.set('condition', conditionHandler);
+    this.handlers.set('variable', variableHandler);
+    this.handlers.set('slack', slackHandler);
+    this.handlers.set('email', emailHandler);
+    this.handlers.set('sheets', sheetsHandler);
+    this.handlers.set('webhook', webhookHandler);
+  }
+
+  getHandler(nodeType: string): INodeHandler | null {
+    return this.handlers.get(nodeType) ?? null;
+  }
+
+  hasHandler(nodeType: string): boolean {
+    return this.handlers.has(nodeType);
+  }
+}
+
+
